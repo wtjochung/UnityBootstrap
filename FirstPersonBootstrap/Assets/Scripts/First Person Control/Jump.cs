d09f6aa3b53eaf8e7547bc2a84059cce6cdcbee5
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2021-10-27 13:57:31 -0700

    Unity Bootstrap

---
+++ FirstPersonBootstrap/Assets/Scripts/First Person Control/Jump.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider), typeof(Rigidbody))]
+public class Jump : MonoBehaviour
+{
+    public float jumpStrength = 5;
+    public KeyCode jumpKey = KeyCode.Space;
+
+    private Rigidbody rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            rb.AddForce(rb.transform.up * jumpStrength, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
---
+++ FirstPersonBootstrap/Assets/Scripts/First Person Control/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider),typeof(Rigidbody))]
+public class Move : MonoBehaviour
+{
+    public float walkSpeed = 5;
+    public float runSpeed = 10;
+    public KeyCode runKey = KeyCode.LeftShift;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float speed = Input.GetKey(runKey) ? runSpeed : walkSpeed;
+
+        float inputX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float inputZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        rb.transform.Translate(inputX, 0, inputZ);
+    }
+}
